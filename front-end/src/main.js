e02c3b9c425e92ba64a108c44d0e206f4ee4ed61
TurukTheCook <yoann.roule@gmail.com>
2018-03-25 00:39:45 +0100

    added simple progress bar

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -6,8 +6,10 @@ import router from './router'
 import { SSL_OP_COOKIE_EXCHANGE } from 'constants';
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import { loadProgressBar } from 'axios-progress-bar'
 
 Vue.use(VueAxios, axios)
+loadProgressBar()
 
 // Intercepteur de requetes HTTP, pour chaque requete http on defini un header
 // qui se nomme 'Authorization' et qui contient le token (depuis le localstorage)
