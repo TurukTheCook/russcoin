07608a0c2d02438cd7b38a207d98ebf71e9918c2
TurukTheCook <yoann.roule@gmail.com>
2018-03-27 11:02:13 +0200

    final gmail styling

--- front-end/src/components/home/messages/Messages.vue
+++ front-end/src/components/home/messages/Messages.vue
@@ -1,14 +1,13 @@
 <template>
 <div>
     <div v-if="$route.name == 'home.messages'" class="m-1 flex flex-column flex-wrap">
-        <div class="message-card list mb-1 border-1 md-elevation-3" v-for="msg in messages" v-on:click="viewMessage(msg._id)">
-            <div class="message-card_header p-1">
-                <span v-bind:class="{'md-caption': msg.read}" class="main-font text-white">SENDER: </span><span class="main-color main-font">{{msg.senderId}}</span>
-            </div>
-            <div class="p-1" :class="[{'unread': !msg.read}, 'read']">
-                <span v-bind:class="{'md-caption': msg.read}" class="main-color">Title: </span><span >{{msg.title}}</span><br/>
-                <span v-bind:class="{'md-caption': msg.read}" class="main-color">Send date: </span><span>{{msg.creationDate | moment}} ({{msg.creationDate | momentFromNow}})</span>
-                <span v-bind:class="{'md-caption': msg.read}" class="main-color" v-if="msg.read">Read Date: </span><span v-if="msg.read">{{msg.readDate | moment}} ({{msg.readDate | momentFromNow}})</span>
+        <div class="message-card cursor-pointer mb-1 border-1 md-elevation-3" v-for="msg in messages" v-on:click="viewMessage(msg._id)">
+            <div class="p-1 flex messages-list" :class="[{'unread': !msg.read}, 'read']">
+                <p class="min-max-100 main-font">{{msg.senderId}}</p>
+                <p class="min-max-100">{{msg.title}}</p>
+                <p class="md-caption flex-grow">{{msg.content}}</p>
+                <p class="min-max-180 text-center">{{msg.creationDate | moment}} ({{msg.creationDate | momentFromNow}})</p>
+                <!-- <p class="px-1" style="min-width: 100px; max-width: 100px; height: 1.5em; overflow: hidden" v-if="msg.read">{{msg.readDate | moment}} ({{msg.readDate | momentFromNow}})</p> -->
             </div>
         </div>
         <!-- PRIMARY ALERTS -->
@@ -53,7 +52,7 @@ export default {
   },
   filters: {
       moment: function(date) {
-          return moment(date).format('MM/DD/YYYY [at] HH:mm:ss')
+          return moment(date).format('Do MMMM')
       },
       momentFromNow: function(date) {
           return moment(date).fromNow()
