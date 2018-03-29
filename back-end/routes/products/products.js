b36535b8726584c99f611330171efb9214292f97
Sharyask <Sharyask@gmail.com>
2018-03-29 14:43:15 +0200

    Start creation of routes "products"

---
+++ back-end/routes/products/products.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import mongoose from 'mongoose'
+
+let products = express.Router();
+
+products.get('/', (req, res) => {
+
+})
+
+products.get('/:id', (req, res) =>{
+
+})
+
+products.post('/', (req, res) => {
+
+})
